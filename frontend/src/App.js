926aae7a64aed524a0bf4e7fd4dbd752ab1e4b95
R4vage <julianmarc@gmail.com>
2022-03-29 13:46:58 -0300

    Creacion de User Page, agregado User a App.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Nosotros from "./Pages/Nosotros";
+import User from "./Pages/User"
 
 function App() {
   return (
     <div className="App">
       <Nosotros />
+      <User />
     </div>
   );
 }
