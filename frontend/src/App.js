417ad396ecbed786d0c8d536ce132994aafb821f
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-03-30 16:00:57 -0400

    Analytics changes, conflicts resolving in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Nosotros from "./Pages/Nosotros";
 import UserAnalytics from "./Pages/UserAnalytics";
+import Analytics from "./Pages/Analytics";
+
 
 function App() {
   return (
     <div className="App">
       <Nosotros />
       <UserAnalytics/>
+      <Analytics />
     </div>
   );
 }
