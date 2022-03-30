315830ff4f48fe39cd25ddf699d2a3803dc119aa
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-03-29 22:41:18 -0400

    Analytics added to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Nosotros from "./Pages/Nosotros";
+import Analytics from "./Pages/Analytics";
+
 
 function App() {
   return (
     <div className="App">
       <Nosotros />
+      <Analytics />
     </div>
   );
 }
