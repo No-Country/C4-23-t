9ae33c5105654658c7d101f41538d27cddec3be6
Pedro Acosta <yandalar@gmail.com>
2022-04-03 11:31:53 -0300

    Creation of Payments page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import Nosotros from "./Pages/Nosotros";
 import UserAnalytics from "./Pages/UserAnalytics";
 import Analytics from "./Pages/Analytics";
-
+import Payments from "./Pages/Payments";
 
 function App() {
   return (
     <div className="App">
       <Nosotros />
-      <UserAnalytics/>
+      <UserAnalytics />
       <Analytics />
+      <Payments />
     </div>
   );
-};
+}
 
 export default App;
