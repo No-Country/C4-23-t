ee482467a8a0aee78ccd14624b75e75a064bb286
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-04-06 14:30:13 -0400

    UserCard Modified

--- frontend/src/Components/UserCard.jsx
+++ frontend/src/Components/UserCard.jsx
@@ -3,26 +3,25 @@ import React from 'react'
 const UserCard = () => {
   return (
     //UserCard
-    <div class="max-w-md mx-auto bg-gray-300 rounded-2xl shadow-md sm:max-w-auto md:max-w-auto lg:max-w-auto xl:max-w-auto sm:h-auto">
-      <div class="md:flex lg:flex xl:flex">
-        <div class="grid grid-cols-2 md:shrink">
+    <div className="max-w-sm mx-auto sm:max-w-auto md:max-w-auto lg:max-w-auto xl:max-w-auto sm:h-auto md:h-auto">
+      <div className="md:flex lg:flex xl:flex">
+        <div className="grid grid-cols-2 md:shrink bg-[#EEEEEE] rounded-3xl">
           <img
-            class="my-6 ml-6 h-32 w-52 object-cover md:h-auto lg:h-auto xl:h-auto md:w-auto lg:w-auto xl:w-auto rounded-[2rem]"
-            src="https://thinkingheads.com/wp-content/uploads/2017/06/ceo.jpg"
+            className="my-6 mx-8 h-32 w-40 object-cover md:h-auto lg:h-auto xl:h-auto md:w-40 lg:w-40 xl:w-40 rounded-[2rem]"
+            src="https://cdn.create.vista.com/api/media/small/204680084/stock-photo-handsome-focused-businessman-sitting-armchair"
             alt="CEO"
           />
-          <div class="p-4 mt-6 mr-2 ml-9 sm:text-center md:text-center lg:text-center xl:text-center">
-            <h1 class="text-[1.3rem] font-['Inder', sans-serif] text-center leading-thight font-medium text-[#1A69B4] hover:underline decoration-blue-600">
+          <div className="p-6 pb-0 my-4 mr-2 sm:text-center md:text-center lg:text-center xl:text-center">
+            <h1 className="text-base font-['Inder', sans-serif] text-center text-[#1A69B4] font-semibold hover:underline decoration-blue-600">
               <q>La seguridad y la estabilidad es lo mejor de una empresa</q>
             </h1>
           </div>
-          <div class="ml-10 mb-8 text-center uppercase tracking-wide text-xl text-[#1A69B4] font-semibold">
+          <div className="text-center pt-0 pb-8 pl-8 items-center uppercase tracking-wider text-2xl text-[#1A69B4] font-semibold">
             CEO
           </div>
         </div>
       </div>
     </div>
-    
   )
 };
 
