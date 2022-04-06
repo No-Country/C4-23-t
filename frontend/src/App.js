cc2f1f6c206ed8c2423a23ef05ea48e91147a699
Pedro Acosta <yandalar@gmail.com>
2022-04-06 13:37:48 -0300

    Initial styling for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import NavBar from "./Components/NavBar";
 import Home from "./Pages/Home";
 import Error from "./Pages/Error";
 import { Route, Routes } from "react-router-dom";
+import LoginForm from "./Pages/Login";
 
 function App() {
   return (
@@ -22,7 +23,9 @@ function App() {
       </Routes>
       <UserAnalytics />
       <Payments />
+      <LoginForm />
     </>
+    </div>
   );
 }
 
