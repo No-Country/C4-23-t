140f6a7f9fc7be771d48f7dd92456d15b3abc28a
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-04-08 01:35:53 -0400

    Fixing src Navbar and Footer

--- frontend/src/Components/Layout.js
+++ frontend/src/Components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
+import Navbar from './NavBar'
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
