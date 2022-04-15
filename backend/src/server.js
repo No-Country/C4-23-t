c505a844e647fa3a9636387168b034499c823842
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-04-15 17:30:51 -0300

    Login, Register

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,28 +5,29 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import connectDb from "./config/db.js";
+import signUp from "./routes/signUp.js";
+import dotenv from "dotenv";
+import singIn from "./routes/singIn.js";
 
 // Creating express app
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 // connect to the Db
 connectDb();
-
 const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}...`);
+});
+
 app.get("/", (req, res) => {
-  res.send(`Server running on port ${port}`);
+  res.send("welcome to the todos api...");
 });
 
-// Error handlers
-app.use(function fourOhFourHandler(req, res) {
-  res.status(404).send();
-});
-
-app.use(function fiveHundredHandler(err, req, res, next) {
-  console.error(err);
-  res.status(500).send();
-});
+//routes
+app.use("/api/singup", signUp);
+app.use("/api/signin", singIn);
 
 // Starting server
-app.listen(port, () => console.log(`Server Running on port ${4000}`));
