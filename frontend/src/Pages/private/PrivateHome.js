6e06ba78f0f5b0508f2c2cd220ee79ea4654e89f
R4vage <julianmarc@gmail.com>
2022-04-17 20:42:36 -0300

    Agregada apis y coins

--- frontend/src/Pages/private/PrivateHome.js
+++ frontend/src/Pages/private/PrivateHome.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect} from "react";
 import logoHome from "../../Assets/logoHome.svg";
 import "../../CSS/PrivateHome.css";
 import UserHomeCard from "../../Components/UserHomeCard";
@@ -6,34 +7,64 @@ import useTitle from "../../Utils/useTitle";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const PrivateHome = () => {
-  useTitle("Home");
 
+const PrivateHome = () => {
+  useTitle("Home")
   const auth = useSelector((state) => state.auth);
-  if (auth.token == null) return <Navigate to="/userLogin" />;
+  const [coinOne, getCoinOne] = useState([])
+  const [coinTwo, getCoinTwo] = useState([])
+  const [coinThree, getCoinThree] = useState([])
+  const APITrending = "https://api.coingecko.com/api/v3/search/trending"
+  const fetchPost = async () => {
+    const response = await fetch(APITrending)
+    if (!response.ok){
+      throw new Error ("Data could not be fetched")
+    } else {
+      return response.json()
+    }
+  }
+  useEffect (() => {
+    fetchPost()
+    .then((res) => {
+      getCoinOne(res.coins.slice(0-3).map((data) =>
+      data.item)[0])
+      getCoinTwo(res.coins.slice(0-3).map((data) =>
+      data.item)[1])
+      getCoinThree(res.coins.slice(0-3).map((data) =>
+      data.item)[2])
+      })
+    .catch((e) => {
+      
+    })
+  }, [])
+  
+
+
+console.log(coinOne)
+console.log(coinTwo)
+console.log(coinThree.name)
+
+
+/* const coinOne = post[0]
+const coinTwo = post[1]
+const coinThree = post[2] */
+
+/* console.log(coinOne)
+console.log(coinTwo)
+console.log(coinThree) */
+
+
+if (auth.token == null) return <Navigate to="/userLogin" />;
+
 
   return (
+    
     <div className="PrivateHome">
       <h3 className="privateHomeTitle">Tendencias</h3>
       <div className="privateHomeContainer">
-        <UserHomeCard
-          cryptoCoin="Bitcoin"
-          price="1000.121"
-          variation="26"
-          graphic={demoGraphic}
-        />
-        <UserHomeCard
-          cryptoCoin="Bitcoin"
-          price="1000.121"
-          variation="26"
-          graphic={demoGraphic}
-        />
-        <UserHomeCard
-          cryptoCoin="Bitcoin"
-          price="1000.121"
-          variation="26"
-          graphic={demoGraphic}
-        />
+        <UserHomeCard cryptoCoin={coinOne.symbol} price={coinOne.price_btc} variation="26" graphic={demoGraphic}/>
+        <UserHomeCard cryptoCoin={coinTwo.symbol} price={coinTwo.price_btc} variation="26" graphic={demoGraphic}/>
+        <UserHomeCard cryptoCoin={coinThree.symbol} price={coinThree.price_btc} variation="26" graphic={demoGraphic}/>
       </div>
       <h3 className="title">Noticias</h3>
       <h4 className="subtitle">Proximamente......</h4>
