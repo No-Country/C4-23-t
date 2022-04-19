cff6e67d8c83a219c5a9e17ea0adfdc25dfee909
Eliseo Falcon <eliseonop@gmail.com>
2022-04-19 15:29:47 -0500

    update

--- frontend/src/Components/CoinRow.js
+++ frontend/src/Components/CoinRow.js
@@ -2,14 +2,22 @@ import React from "react";
 
 const CoinRow = ({ coin, index }) => {
   return (
-    <tr className="  divide-x divide-slate-500">
+    <tr className=" h-5 divide-x divide-slate-500 border-sky-300 text-center">
       <td className="text-muted font-bold">{index}</td>
       <td>
-        <img src={coin.image} alt="" className=" rounded-full w-6" />
+        <img
+          src={coin.image}
+          alt=""
+          className=" rounded-full w-6 block m-auto"
+        />
       </td>
       <td>
-        <span className="font-bold">{coin.name}</span>
-        <span className="w-2 ml-7 text-indigo-300">{coin.symbol}</span>
+        <span className="font-bold text-blue-700">{coin.name}</span>
+      </td>
+      <td>
+        <span className="w-2 ml-7 text-black-300 font-bold">
+          {coin.symbol.toUpperCase()}
+        </span>
       </td>
 
       <td>${coin.current_price.toLocaleString()}</td>
@@ -17,8 +25,8 @@ const CoinRow = ({ coin, index }) => {
       <td
         className={
           coin.price_change_percentage_24h > 0
-            ? "text-green-500 font-bold"
-            : "text-red-700 font-bold"
+            ? "text-blue-700 font-bold bg-green-200"
+            : "text-blue-700 font-bold bg-red-300"
         }
       >
         {coin.price_change_percentage_24h}
--- frontend/src/Components/SearchCoin.js
+++ frontend/src/Components/SearchCoin.js
@@ -22,7 +22,7 @@ const SearchCoin = () => {
   }, []);
 
   return (
-    <div className="border-4">
+    <div className="border-4 ">
       <div className="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
         <form class="group relative">
           <svg
