9e7acaea8e6fc87829d829fa372009c39a420c97
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-04-20 04:01:51 -0400

    fix: changing styles to slidercard section

--- frontend/src/Components/SectionAboutUs.jsx
+++ frontend/src/Components/SectionAboutUs.jsx
@@ -17,17 +17,17 @@ const SectionAboutUs = () => {
             Sé parte de nuestra Exchange y empieza a invertir a lo grande.
           </p>
           <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-            <a rel="noopener noreferrer" href={"Analytics"} className="px-8 py-3 text-lg font-semibold rounded-md hover:text-xl shadow-lg dark:bg-sky-600 dark:text-white" >
+            <a rel="noopener noreferrer" href={"userLogin"} className="px-8 py-3 text-lg font-semibold rounded-md hover:text-xl shadow-lg dark:bg-sky-600 dark:text-white" >
               Sign Up</a>
             <a href={"Analytics"} rel="noopener noreferrer" className="px-8 py-3 text-lg font-semibold border-2 hover:text-xl hover:bg-[#1A69B4] hover:text-white shadow-lg rounded-md text-sky-600">
               Analytics</a>
           </div>
         </div>
-        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+        <div className="flex items-center justify-center p-1 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
           <img
             src="https://cdn.goconqr.com/uploads/media/image/31223372/desktop_d7b48597-53f0-4457-911d-b322f7fade11.png"
             alt="TeamWork"
-            className="object-cover h-72 w- sm:h-80 lg:h-96 xl:h-112 2xl:h-128 w-auto"
+            className="object-cover sm:h-80 lg:h-96 xl:h-112 2xl:h-128 w-screen "
           />
         </div>
       </div>
--- frontend/src/Components/SliderCard.js
+++ frontend/src/Components/SliderCard.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import UserCard from "../Components/UserCard";
-import imgSlider from "../Assets/imgSlider.png";
+import imgSlider from "../Assets/fondo-azul-202668.jpg";
+import imgPedro from '../Assets/pedroacosta.png'
+import imgPablo from '../Assets/pablovyeira.png'
+import imgJulian from '../Assets/julianmarc.jpg'
+import imgMartin from '../Assets/martincastro.jpg'
+import imgEdu from '../Assets/martincastro.jpg'
+import imgMiguel from '../Assets/martincastro.jpg'
 import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons";
 
 export default function SliderCard() {
@@ -23,29 +29,29 @@ export default function SliderCard() {
         <img
           src={imgSlider}
           alt="fondo"
-          className="w-screen h-[350px] sm:h-screen shadow-lg rounded-[40px] "
+          className="w-screen h-[350px] sm:h-screen shadow-lg "
         />
         <h1 className="absolute top-5 sm:top-20 text-white font-bold text-lg sm:text-4xl  text-center px-4  ">
-          ¿Quienes son los integrantes de nuestro equipo?
+          ¿Quiénes son los integrantes de nuestro equipo?
         </h1>
         {/* mobil */}
         <div className="snap-x flex w-full h-full sm:hidden gap-6 overflow-x-auto sm:overflow-x-hidden absolute pt-20 sm:">
           <div className="ml-4 shrink-0 snap-center overflow-hidden ">
-            <UserCard puesto={"Developer"} />
+            <UserCard dev={"Developer"} />
           </div>
           <div className="ml-2 shrink-0 snap-center overflow-hidden">
-            <UserCard puesto={"CEO"} />
+            <UserCard dev={"CEO"} />
           </div>
           <div className="ml-2 shrink-0 snap-center overflow-hidden">
-            <UserCard puesto={"ADMIN"} />
+            <UserCard dev={"ADMIN"} />
           </div>
           <div className="ml-2 shrink-0 snap-center overflow-hidden">
-            <UserCard puesto={"Marketing"} />
+            <UserCard dev={"Marketing"} />
           </div>
         </div>
         {/* sm:max */}
-        <div className="hidden sm:flex sm:flex-col absolute  sm:items-center sm:gap-6 ">
-          <div className="   top-1/4 right-1/3 md:top-4 md:right-8 md:absolute">
+        <div className="hidden sm:flex sm:flex-col absolute sm:items-center sm:gap-6 ">
+          <div className="top-1/4 right-1/3 md:top-4 md:right-8 md:absolute">
             <button className=" text-white font-bold py-2 px-4 rounded-full">
               <ArrowBackIcon w={30} h={30} boxSize={80} />
             </button>
@@ -57,22 +63,19 @@ export default function SliderCard() {
               <ArrowForwardIcon w={30} h={30} boxSize={80} />
             </button>
           </div>
-          <div
-            className={next ? "md:translate-x-[-200px] shadow-2xl " : " hidden"}
-          >
-            <UserCard puesto={"Developer"} />
+          <div className={next ? "md:translate-x-[-200px] shadow-2xl" : "hidden"}>
+            <UserCard dev={"FULLSTACK DEV"} developer={imgPedro} text={"La comunicación y el trabajo en equipo es fundamental para llevar a cabo un proyecto."} />
           </div>
-          <div className={next ? " md:ml-96 shadow-2xl" : " hidden"}>
-            <UserCard puesto={"CEO"} />
+          <div className={next ? "md:ml-96 shadow-2xl" : "hidden"}>
+            <UserCard dev={"FULLSTACK DEV"} developer={imgPablo} text={"La dedicación y la constancia son claves para el éxito."} />
           </div>
-          <div
-            className={next ? " hidden" : "md:translate-x-[-200px] shadow-2xl"}
-          >
-            <UserCard puesto={"ADMIN"} />
+          <div className={next ? "hidden" : "md:translate-x-[-200px] shadow-2xl"}>
+            <UserCard dev={"FRONTEND TESTER"} developer={imgJulian} text={"Un buen desarrollo y buena estética hacen brillar cualquier proyecto."} />
           </div>
-          <div className={next ? " hidden" : "md:ml-96  shadow-2xl"}>
-            <UserCard puesto={"Marketing"} />
+          <div className={next ? " hidden" : "md:ml-96 shadow-2xl"}>
+            <UserCard dev={"BACKEND DEV"} developer={imgMartin} text={"Aprender de otros desarrolladores es un privilegio."} />
           </div>
+          
         </div>
       </div>
     </section>
