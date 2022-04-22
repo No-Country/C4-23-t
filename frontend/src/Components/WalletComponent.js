850994e2dc59efb16a101a14e5e1a99bdf8f0d55
Pedro Acosta <yandalar@gmail.com>
2022-04-21 23:16:28 -0300

    Fixed somethings on wallet and buy pesos function

--- frontend/src/Components/WalletComponent.js
+++ frontend/src/Components/WalletComponent.js
@@ -3,22 +3,15 @@ import { useEffect, useState } from "react";
 import "../CSS/WalletComponent.css";
 import signoPeso from "../Assets/signo_pesos.png";
 
-const Wallet = () => {
-  const walletPrueba = {
-    ars: 0,
-    btc: 0.0012,
-    eth: 2,
-    bnb: 13,
-    usdt: 0,
-    usdc: 0,
-  };
-
+const Wallet = ({ datos, setDatos }) => {
   const [coins, setCoins] = useState([]);
+  const [pesos, setPesos] = useState(0);
+  const [inputPesos, setInputPesos] = useState(0);
 
   const getData = async () => {
     try {
       const res = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ars&order=market_cap_desc&per_page=10&page=1"
       );
       setCoins(res.data);
       console.log(res.data);
@@ -27,9 +20,22 @@ const Wallet = () => {
     }
   };
 
+  const onButtonClick = (e) => {
+    e.preventDefault();
+    const value = parseInt(inputPesos) + parseInt(pesos);
+    setPesos(value);
+    setInputPesos(0);
+  };
+
+  const addPesos = (e) => {
+    setInputPesos(e.target.value);
+  };
+
   useEffect(() => {
     getData();
   }, []);
+
+  console.log(pesos);
 
   return (
     <div className="walletContainer">
@@ -49,19 +55,29 @@ const Wallet = () => {
               </div>
               <div className="walletCoinColumnName">Pesos</div>
             </td>
+            <td className="walletBalance">
+              <div className="walletBalanceAmount">
+                {datos[0] && datos[0].arg}
+              </div>
+            </td>
+            <td>
+              <button id="walletPesosButton">Ingresar $</button>
+            </td>
           </tr>
           {coins.map((row) => {
-            console.log(walletPrueba.hasOwnProperty(row.symbol));
-            return walletPrueba.hasOwnProperty(row.symbol) ? (
+            return datos[0].hasOwnProperty(row.symbol) ? (
               <tr className="walletBody">
-                <td className="walletCoinColumn">
+                <td className="walletCoinColumn" id="walletCoins">
                   <div className="walletCoinColumnImg">
                     <img src={row.image} alt="coin logo" />
                   </div>
                   <div className="walletCoinColumnName">{row.name}</div>
                 </td>
                 <td className="walletBalance">
-                  <div>${row.current_price.toLocaleString()}</div>
+                  <div>{datos[0][row.symbol]}</div>
+                  <div id="walletBalanceAmount">
+                    = ${datos[0][row.symbol] * row.current_price}
+                  </div>
                 </td>
                 <td>
                   <button>Comprar {row.symbol.toUpperCase()}</button>
@@ -71,6 +87,17 @@ const Wallet = () => {
           })}
         </tbody>
       </table>
+      <div className="buyPesos">
+        <form>
+          <input
+            type="number"
+            placeholder="Ingrese monto"
+            onChange={addPesos}
+            value={inputPesos}
+          />
+          <button onClick={onButtonClick}>Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
